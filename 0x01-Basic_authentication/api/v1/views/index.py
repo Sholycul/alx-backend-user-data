13363b77ea6bc01fd24fcf0d52a439e0df6b6745
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-09-11 18:47:42 -0700

    Handle unauthorize access

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized():
+    """Raises a 401 error to trigger the error handler"""
+    abort(401)
