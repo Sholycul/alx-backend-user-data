80d0fdd9e0fa8c39d7a62ccb3141a41d4f8d1da4
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-09-16 18:52:23 -0700

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
-
+"""Authentication module.
+"""
 from flask import request
 from typing import List, TypeVar
+import fnmatch
+
 
 class Auth:
-    """Template for all authentication systems."""
-
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """For now, return False, logic will be added later."""
-        return False
+        """ Method to check if auth is required.
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """For now, return None, will implement logic later."""
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """For now, return None, will implement logic later."""
+        """ Method to get user from request.
+        """
         return None
-
